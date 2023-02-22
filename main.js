fa58cab76e7bf94c2b680ee67f2135d6e5143d68
Дарига Арапбаева <darigaarapbaeva@MacBook-Air-Dariga-2.local>
2023-02-22 10:34:33 +0600

    lab5

---
+++ main.js
@@ -0,0 +1,25 @@
+alert ('test')
+function load ()
+
+let a = [1,2,3,4,5];
+
+//let column_1 = [9, 1,2, ...J;
+
+//let column_2 = [9,1,2, ...];
+
+//let column_3 = [9, 1,2,...];
+
+//let column_4 = [9, 1,2, ...];
+
+//sum = a [0] + a[l] + a[2] + a[3] + a [4];
+
+sum = 0;
+
+for (i = O;i<a.length;i++) {
+sum = sum + alil;
+}
+/alert ("Sum==" + sum);
+const result = {
+sumTotal: sum, column 1: a
+}
+console.log (result)
